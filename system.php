7a0c42e972d662eb2f06307158f55bc8e951fdd3
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-09-21 19:39:31 +0100

    Changing initialisation of system to a dedicated file and more message system work

---
+++ system.php
@@ -0,0 +1,42 @@
+<?php
+require_once("config.php");
+require_once('messagepump.php');
+require_once('model-bbox.php');
+require_once('modelfactory.php');
+
+$messagePump->AddListener(Message::MAP_QUERY, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_OBJECT_BY_ID, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_FULL_HISTORY, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_RELATIONS_FOR_ELEMENT, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_WAYS_FOR_NODE, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::CHECK_ELEMENT_EXISTS, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_CURRENT_ELEMENT_VER, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_ELEMENT_BBOX, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::CREATE_ELEMENT, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::MODIFY_ELEMENT, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::DELETE_ELEMENT, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::DUMP, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::PURGE_MAP, "MapDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::SCRIPT_END, "MapDatabaseEventHandler", Null);
+
+$messagePump->AddListener(Message::CHANGESET_IS_OPEN, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::OPEN_CHANGESET, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::UPDATE_CHANGESET, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::CLOSE_CHANGESET, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_CHANGESET_UID, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_CHANGESET_METADATA, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_CHANGESET_SIZE, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::EXPAND_BBOX, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::CHANGESET_APPEND_ELEMENT, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::CHANGESET_QUERY, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_CHANGESET_CONTENT, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::GET_CHANGESET_CLOSE_TIME, "ChangesetDatabaseEventHandler", Null);
+$messagePump->AddListener(Message::SCRIPT_END, "ChangesetDatabaseEventHandler", Null);
+
+$messagePump->AddListener(Message::XAPI_QUERY, "ModelBboxEventHandler", Null);
+$messagePump->AddListener(Message::CREATE_ELEMENT, "ModelBboxEventHandler", Null);
+$messagePump->AddListener(Message::MODIFY_ELEMENT, "ModelBboxEventHandler", Null);
+$messagePump->AddListener(Message::DELETE_ELEMENT, "ModelBboxEventHandler", Null);
+$messagePump->AddListener(Message::SCRIPT_END, "ModelBboxEventHandler", Null);
+
+?>
