11faea9c2853818d5e2db6e8eb5949949df4f4eb
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-10-30 22:37:25 +0000

    OAuth request token working with toy settings

--- oauthmicrocosm.php
+++ oauthmicrocosm.php
@@ -3,31 +3,90 @@ require_once('OAuth.php');
 require_once('fileutils.php');
 require_once('system.php');
 
+class OAuthMicrocosmStore extends OAuthDataStore
+{
+	function __construct()
+	{
+		$this->consumer = new OAuthConsumer("AdCRxTpvnbmfV8aPqrTLyA", "XmYOiGY9hApytcBC3xCec3e28QBqOWz5g6DSb5UpE", NULL);
+		$this->requestToken = new OAuthToken("requestkey", "requestsecret", 1);
+	}
+
+	function lookup_consumer($consumer_key) 
+	{
+		if ($consumer_key == $this->consumer->key) 
+			return $this->consumer;
+		return NULL;
+	}
+
+	function lookup_token($consumer, $token_type, $token)
+	{
+		// implement me
+	}
+
+	function lookup_nonce($consumer, $token, $nonce, $timestamp)
+	{
+		// implement me
+	}
+
+	function new_request_token($consumer, $callback = null)
+	{
+		// return a new token attached to this consumer
+		if ($consumer->key == $this->consumer->key or True)
+		{
+			return $this->requestToken;
+		}
+		return NULL;
+	}
+
+	function new_access_token($token, $consumer, $verifier = null)
+	{
+		// return a new access token attached to this consumer
+		// for the user associated with this token if the request token
+		// is authorized
+		// should also invalidate the request token
+	}
+}
+
+
 class OAuthMicrocosm
 {
 	function __construct()
 	{
-		$this->dataStore = new SimpleOAuthDataStore();
+		$this->dataStore = new OAuthMicrocosmStore();
 	}
 
 	function lookupConsumer($provider)
 	{
-		//Hard coded JOSM keys
-		if($provider != "AdCRxTpvnbmfV8aPqrTLyA")
-			return OAUTH_CONSUMER_KEY_UNKNOWN;
-		//return OAUTH_CONSUMER_KEY_REFUSED;
-		$provider->consumer_secret = "XmYOiGY9hApytcBC3xCec3e28QBqOWz5g6DSb5UpE";
 
-		return OAUTH_OK;
 	}
 	
 	function RequestToken()
 	{
-		$server = new OAuthServer();
-		$token = $server->requestToken();
-		echo $token;
-	}
+		try
+		{
+			$server = new OAuthServer($this->dataStore);
+			$server->add_signature_method(new OAuthSignatureMethod_HMAC_SHA1());
+			$req = OAuthRequest::from_request();
+			$token = $server->fetch_request_token($req);
 
+			echo $token;
+			$fi = fopen("out.txt","wt");
+			fwrite($fi, $token);
+			fclose($fi);
+		}
+		catch (OAuthException $e)
+		{
+			print($e->getMessage() . "\n<hr />\n");
+			print_r($req);
+
+			$fi = fopen("out.txt","wt");
+			fwrite($fi, $e->getMessage() . "\n<hr />\n");
+			fwrite($fi, print_r($req, True));
+			fclose($fi);
+
+			die();
+		}
+	}
 }
 
 //Split URL for processing
@@ -37,7 +96,6 @@ $urlExp = explode("/",$pathInfo);
 $fi = fopen("log.txt","wt");
 fwrite($fi,print_r($_POST,True));
 fwrite($fi,print_r($pathInfo,True));
-
 
 if($urlExp[1] == "request_token")
 {
